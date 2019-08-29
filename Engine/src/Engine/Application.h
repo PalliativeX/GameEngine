a451bc3c25fdcf314180395a021444a299d11da5
PalliativeX <vvovvan009@yandex.ru>
2019-08-29 16:18:17 +0300

    set up basic application and entry point

---
+++ Engine/src/Engine/Application.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include "Core.h"
+
+namespace Engine {
+
+	class ENGINE_API Application
+	{
+	public:
+		Application();
+		virtual ~Application();
+
+		void run();
+	};
+
+	// to be defined in CLIENT
+	Application* createApplication();
+
+}
