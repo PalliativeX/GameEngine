5efce4a23eff76403d02bb343bfb10c47fa2fffc
PalliativeX <vvovvan009@yandex.ru>
2019-08-29 17:52:35 +0300

    set up a logging system

--- Engine/src/Engine/EntryPoint.h
+++ Engine/src/Engine/EntryPoint.h
@@ -6,6 +6,11 @@ extern Engine::Application* Engine::createApplication();
 
 int main(int argc, char** argv)
 {
+	Engine::Log::init();
+	Engine::Log::getCoreLogger()->warn("Initialized Log!");
+	Engine::Log::getClientLogger()->info("Hello!");
+
+
 	auto app = Engine::createApplication();
 	app->run();
 	delete app;
---
+++ Engine/src/Engine/Log.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <memory> 
+
+#include "Core.h"
+#include "spdlog/spdlog.h"
+
+namespace Engine {
+
+	class ENGINE_API Log
+	{
+	public:
+		static void init();
+
+		inline static std::shared_ptr<spdlog::logger>& getCoreLogger() { return s_CoreLogger; }
+		inline static std::shared_ptr<spdlog::logger>& getClientLogger() { return s_ClientLogger; }
+
+	private:
+		static std::shared_ptr<spdlog::logger> s_CoreLogger;
+		static std::shared_ptr<spdlog::logger> s_ClientLogger;
+	};
+
+}
